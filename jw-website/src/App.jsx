812db3955aa097dbcaa5f10503fc2c0e48f175ea
Ashley Witkowski <ashwitkowski@gmail.com>
2019-09-19 14:33:27 -0400

    fixed

--- jw-website/src/App.jsx
+++ jw-website/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
-import Reel from "./components/Reel/Reel";
+import Reel from "./components/Reel/reel";
 import NavBar from "./components/NavBar/NavBar";
 import test from "./components/About/About";
 import Photos from "./components/Photos/Photos";
